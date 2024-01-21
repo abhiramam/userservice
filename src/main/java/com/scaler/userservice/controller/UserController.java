465b043fa227e74cddbf69b825c781e6f64846b9
Abhiram Ambadipudi <abhiramambadipudi4444@gmail.com>
2024-01-21 17:24:35 +0530

    Initial commit

---
+++ src/main/java/com/scaler/userservice/controller/UserController.java
@@ -0,0 +1,4 @@
+package com.scaler.userservice.controller;
+
+public class UserController {
+}
---
+++ src/main/java/com/scaler/userservice/modal/User.java
@@ -0,0 +1,23 @@
+package com.scaler.userservice.modal;
+
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import lombok.Getter;
+import lombok.Setter;
+import org.springframework.boot.autoconfigure.web.WebProperties;
+
+@Getter
+@Setter
+@Entity
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+    private String name;
+    private String email;
+    private String password;
+    private boolean isAdmin;
+
+}
